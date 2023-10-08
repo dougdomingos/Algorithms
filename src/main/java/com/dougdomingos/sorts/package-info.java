b742a8053aba2b06f983e4c51c1e4daa0024f51c
Douglas Domingos <doug.domingossilva@gmail.com>
2023-10-08 13:34:31 -0300

    docs(Sorts): add package-info.java to sorts package

---
+++ src/main/java/com/dougdomingos/sorts/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * This package provides a range of sorting algorithms, classified into packages
+ * by their time complexity class.
+ * <p>
+ * The sorting algorithms are classified into packages by their time complexity,
+ * as follows:
+ * <ul>
+ *   <li>Linear: algorithms with worst-case time complexity of O(n)
+ *   <li>Quadratic: algorithms with worst-case time complexity of O(n^2)
+ *   <li>Logarithmic: algorithms with worst-case time complexity of O(n * log n)
+ * </ul>
+ */
+package com.dougdomingos.sorts;
