06b60caca49f6a96affeda28669c04d2ba7daf97
Douglas Domingos <doug.domingossilva@gmail.com>
2023-10-17 18:27:48 -0300

    Structures(BinaryTree): add BST implementation

---
+++ src/main/java/com/dougdomingos/structs/binaryTree/bst/BST.java
@@ -0,0 +1,134 @@
+package com.dougdomingos.structs.binaryTree.bst;
+
+import com.dougdomingos.structs.binaryTree.AbstractBT;
+import com.dougdomingos.structs.binaryTree.node.BTNode;
+
+/**
+ * Implementation of a Binary Search Tree.
+ */
+public class BST<T extends Comparable<T>> extends AbstractBT<T> {
+
+    @Override
+    public void add(T element) {
+        if (element != null) {
+            return;
+        }
+
+        BTNode<T> newNode = new BTNode<T>(element);
+
+        if (isEmpty()) {
+            root = newNode;
+        } else {
+            add(root, newNode);
+        }
+    }
+
+    @Override
+    public void remove(T element) {
+        if (isEmpty()) {
+            return;
+        }
+
+        BTNode<T> target = search(element);
+        if (target != null) {
+            remove(target);
+        }
+    }
+
+    // --------------------- AUXILIAR METHODS ----------------------
+
+    /**
+     * Recursive method for adding a new node into the tree.
+     * 
+     * @param root
+     *            The root of the tree
+     * @param newNode
+     *            The new node to be inserted into the tree
+     */
+    private void add(BTNode<T> root, BTNode<T> newNode) {
+        if (root.isLeaf()) {
+            newNode.setParent(root);
+
+            if (newNode.compareTo(root.getData()) < 0) {
+                root.setLeft(newNode);
+            } else if (newNode.compareTo(root.getData()) > 0) {
+                root.setRight(newNode);
+            }
+        } else {
+            if (newNode.compareTo(root.getData()) < 0) {
+                add(root.getLeft(), newNode);
+            } else if (newNode.compareTo(root.getData()) > 0) {
+                add(root.getRight(), newNode);
+            }
+        }
+    }
+
+    /**
+     * Auxiliar method for removing an element of the tree.
+     * 
+     * @param target
+     *            The node to be removed
+     */
+    private void remove(BTNode<T> target) {
+        if (target.isLeaf()) {
+            // Case 1: The target is a leaf node
+            if (target.equals(root)) {
+                // If the node is the root, simply set it to null
+                root = null;
+            } else {
+                // Otherwise, set its parent reference to null
+                BTNode<T> parent = target.getParent();
+
+                if (parent.getLeft().equals(target)) {
+                    parent.setLeft(null);
+                } else {
+                    parent.setRight(null);
+                }
+            }
+        } else if (target.hasOnlyLeftChild()) {
+            // Case 2.1: The target only has one child (left node)
+            if (target.equals(root)) {
+                // If the node is the root, replace the root with the left child
+                root = target.getLeft();
+                root.setParent(null);
+            } else {
+                // Otherwise, set the parent of the left child to be the parent
+                // of the target and the other way around
+                BTNode<T> parent = target.getParent();
+                target.getLeft().setParent(parent);
+
+                if (target.compareTo(parent.getData()) < 0) {
+                    parent.setLeft(target.getLeft());
+                } else {
+                    parent.setRight(target.getLeft());
+                }
+            }
+        } else if (target.hasOnlyRightChild()) {
+            // Case 2.2: The target only has one child (right node)
+            if (target.equals(root)) {
+                // If the node is the root, replace the root with the left child
+                root = target.getRight();
+                root.setParent(null);
+            } else {
+                // Otherwise, set the parent of the right child to be the parent
+                // of the target and the other way around
+                BTNode<T> parent = target.getParent();
+                target.getRight().setParent(parent);
+
+                if (target.compareTo(parent.getData()) < 0) {
+                    parent.setLeft(target.getRight());
+                } else {
+                    parent.setRight(target.getRight());
+                }
+            }
+        } else {
+            // Case 4: The target has two child nodes
+            // Override the target value with its sucessor, then remove the
+            // sucessor node
+            BTNode<T> sucessor = successorOf(target.getData());
+            target.setData(sucessor.getData());
+            remove(sucessor);
+        }
+
+    }
+}
